ad6d23fecf36407d9d620b30c001f50c19ac622a
arkptz <arkptz@gmail.com>
2024-05-29 01:03:51 +0300

    merge with upstream

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1173,8 +1173,7 @@ with pkgs; {
   fetchNextcloudApp = callPackage ../build-support/fetchnextcloudapp {};
 
   # `fetchurl' downloads a file from the network.
-  fetchurl =
-    if stdenv.buildPlatform != stdenv.hostPlatform
+  fetchurl = if stdenv.buildPlatform != stdenv.hostPlatform
     then buildPackages.fetchurl # No need to do special overrides twice,
     else makeOverridable (import ../build-support/fetchurl) {
       inherit lib stdenvNoCC buildPackages;
@@ -1191,24 +1190,9 @@ with pkgs; {
         perl = buildPackages.perl.override { inherit zlib; fetchurl = stdenv.fetchurlBoot; };
         openssl = buildPackages.openssl.override {
           fetchurl = stdenv.fetchurlBoot;
-          zlib = buildPackages.zlib.override {fetchurl = stdenv.fetchurlBoot;};
-          pkg-config = buildPackages.pkg-config.override (old: {
-            pkg-config = old.pkg-config.override {
+          buildPackages = {
+            coreutils = buildPackages.coreutils.override {
               fetchurl = stdenv.fetchurlBoot;
-            };
-          });
-          perl = buildPackages.perl.override {fetchurl = stdenv.fetchurlBoot;};
-          openssl = buildPackages.openssl.override {
-            fetchurl = stdenv.fetchurlBoot;
-            buildPackages = {
-              coreutils = buildPackages.coreutils.override {
-                fetchurl = stdenv.fetchurlBoot;
-                inherit perl;
-                xz = buildPackages.xz.override {fetchurl = stdenv.fetchurlBoot;};
-                gmp = null;
-                aclSupport = false;
-                attrSupport = false;
-              };
               inherit perl;
               xz = buildPackages.xz.override { fetchurl = stdenv.fetchurlBoot; };
               gmpSupport = false;
@@ -1217,32 +1201,33 @@ with pkgs; {
             };
             inherit perl;
           };
-          libssh2 = buildPackages.libssh2.override {
-            fetchurl = stdenv.fetchurlBoot;
-            inherit zlib openssl;
-          };
-          # On darwin, libkrb5 needs bootstrap_cmds which would require
-          # converting many packages to fetchurl_boot to avoid evaluation cycles.
-          # So turn gssSupport off there, and on Windows.
-          # On other platforms, keep the previous value.
-          gssSupport =
-            if stdenv.isDarwin || stdenv.hostPlatform.isWindows
+          inherit perl;
+        };
+        libssh2 = buildPackages.libssh2.override {
+          fetchurl = stdenv.fetchurlBoot;
+          inherit zlib openssl;
+        };
+        # On darwin, libkrb5 needs bootstrap_cmds which would require
+        # converting many packages to fetchurl_boot to avoid evaluation cycles.
+        # So turn gssSupport off there, and on Windows.
+        # On other platforms, keep the previous value.
+        gssSupport =
+          if stdenv.isDarwin || stdenv.hostPlatform.isWindows
             then false
             else old.gssSupport or true; # `? true` is the default
-          libkrb5 = buildPackages.libkrb5.override {
-            fetchurl = stdenv.fetchurlBoot;
-            inherit pkg-config perl openssl;
-            keyutils = buildPackages.keyutils.override {fetchurl = stdenv.fetchurlBoot;};
-          };
-          nghttp2 = buildPackages.nghttp2.override {
-            fetchurl = stdenv.fetchurlBoot;
-            inherit pkg-config;
-            enableApp = false; # curl just needs libnghttp2
-            enableTests = false; # avoids bringing `cunit` and `tzdata` into scope
-          };
-        });
-      };
-
+        libkrb5 = buildPackages.libkrb5.override {
+          fetchurl = stdenv.fetchurlBoot;
+          inherit pkg-config perl openssl;
+          keyutils = buildPackages.keyutils.override { fetchurl = stdenv.fetchurlBoot; };
+        };
+        nghttp2 = buildPackages.nghttp2.override {
+          fetchurl = stdenv.fetchurlBoot;
+          inherit pkg-config;
+          enableApp = false; # curl just needs libnghttp2
+          enableTests = false; # avoids bringing `cunit` and `tzdata` into scope
+        };
+      });
+    };
   fetchRepoProject = callPackage ../build-support/fetchrepoproject {};
 
   fetchipfs = import ../build-support/fetchipfs {
